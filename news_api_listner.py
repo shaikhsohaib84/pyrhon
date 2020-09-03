26ba46fe535eb8a40f81fb16f8cf9fee30cfddee
sohaib shaikh <shaikhsohaib84@gmail.com>
2020-09-03 16:34:36 +0530

    news listner pythn program added

---
+++ news_api_listner.py
@@ -0,0 +1,35 @@
+import json
+import requests
+from win32com.client import Dispatch
+def json_read(string):
+    req = requests.get(string)
+    req = req.json()
+    for i in range(len(req["articles"])):
+        source_name = req["articles"][i]["source"]["name"]
+        author_name = req["articles"][i]["author"]
+        title_name =  req["articles"][i]["title"]
+        description =  req["articles"][i]["description"]
+        publishedAt = req["articles"][i]["publishedAt"]
+
+        speak(f"{i+1} news")
+        speak("source name is ")
+        speak(source_name)
+
+        speak("author is")
+        speak(author_name)
+
+        speak("today's title ")
+        speak(title_name)
+
+        speak("description")
+        speak(description)
+
+        speak("date of new is")
+        speak(publishedAt)
+
+def speak(str):
+    speak = Dispatch("SAPI.SpVoice")
+    speak.Speak(str)
+
+if __name__ == '__main__':
+    json_read("http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=e36eea905ac847e89e903e9a3ac79dad")
